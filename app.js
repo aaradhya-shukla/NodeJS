f81c908aeabe7f1dcc14131823f5cffbead9c6eb
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-08-09 20:01:25 +0530

    using stream and buffer

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
-// creating server
-const http=require('http');
-const server=http.createServer((req,res)=>{
-    console.log("Aaradhya shukla")
-    let url=req.url;
-    console.log(url)
-    res.setHeader('Content-type','text/html');
-    res.write('<html>')
-    res.write('<head><title>My First Node Js Page</title></head>')
-    if (url==='/home'){
-        res.write('<body><h1>Welcome home</h1></body>')
-    }
-    else if(url==='/about'){
-        res.write('<body><h1>Welcome to About us Page</h1></body>')
-    }
-    else{
-        res.write('<body><h1>Welcome to Node JS Page</h1></body>')
-    }
-    res.write('</html>')
-    res.end();
+const http = require('http');
+const fs = require('fs');
 
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      console.log("now we here")
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    console.log("yella")
+    return res.end(()=>{
+        console.log("end has happend")
+    });
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
-server.listen(4000);
+server.listen(3000);
