0327f28d4be10d0d28fcf56ae58be841f4c65c30
aaradhya-shukla <aaradhya.shukla229@gmail.com>
2023-08-09 15:29:23 +0530

    sending response

--- app.js
+++ app.js
@@ -2,6 +2,23 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
     console.log("Aaradhya shukla")
+    let url=req.url;
+    console.log(url)
+    res.setHeader('Content-type','text/html');
+    res.write('<html>')
+    res.write('<head><title>My First Node Js Page</title></head>')
+    if (url==='/home'){
+        res.write('<body><h1>Welcome home</h1></body>')
+    }
+    else if(url==='/about'){
+        res.write('<body><h1>Welcome to About us Page</h1></body>')
+    }
+    else{
+        res.write('<body><h1>Welcome to Node JS Page</h1></body>')
+    }
+    res.write('</html>')
+    res.end();
+
 });
 
 server.listen(4000);
--- play.js
+++ play.js
@@ -1,7 +1,63 @@
-student={
-    name:'aaradhya',
-    greet(){
-        console.log(`${this}: hi there`)
-    }
+// let promise1=new Promise((resolve,reject)=>{
+
+// });
+// let promise2=new Promise((resolve,reject)=>{});
+// let promise3=new Promise((resolve,reject)=>{});
+// let promise4=new Promise((resolve,reject)=>{});
+// let promise5=new Promise((resolve,reject)=>{});
+// promises
+console.log('a');
+
+console.log('b');
+
+// function c(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(() => {
+//             resolve('c');   
+//         } ,3000)
+//     })
+// }
+
+// function d(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(() => {
+//             resolve('d');
+//         }, 0)
+//     })
+// }
+
+// function e(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(() => {
+//             resolve('e');
+//         }, 0)
+//     })
+// }
+
+// Promise.all([c(),d(),e()]).then((res)=>{for(let i of res){
+//     console.log(i)
+// }})
+// async await
+const cde=async()=>{
+    const c= new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("c");
+        },3000);
+    })
+    console.log(await c);
+    const d=new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("d");
+        },0);
+    })
+    console.log(await d)
+    return("e");
 }
-student.greet()
+cde().then((val)=>console.log(val));
+
+
+
+
+
+
+
